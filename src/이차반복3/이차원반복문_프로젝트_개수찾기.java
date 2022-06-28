ff60e7f106088bf073740494eca901f04f404f48
keduit <keduit@keduit-pc>
2022-06-28 15:41:42 +0900

    이차원반복문_프로젝트_개수찾기 배열4_알고리즘_개수찾기_문제

--- src/이차반복3/이차원반복문_프로젝트_개수찾기.java
+++ src/이차반복3/이차원반복문_프로젝트_개수찾기.java
@@ -14,7 +14,44 @@ public class 이차원반복문_프로젝트_개수찾기 {
 			 답 : 6		
 		 */
 		
+		
+		int[] arr = { 1, 2, 3, 3, 100, 1, 3, 3, 2, 3, 2 };
+		
+		int minCount = arr.length;
+		int maxCount = 0;
+		int maxIndex = -1;
+		int minIndex = -1; 
+		
+		
+		for(int i = 0; i < arr.length; i++)
+		{	
+			int count = 0;
+			for(int j = 0; j < arr.length; j++)
+			{
+				if(arr[i] == arr[j])
+				{
+					count ++;
+				}
 
+			}
+			
+			if(count > maxCount)
+			{
+				maxCount = count;
+				maxIndex = i;
+			}
+			
+			if(count < minCount)
+			{
+				minCount = count;
+				minIndex = i;
+			}
+		}
+		
+		System.out.println(arr[maxIndex] + ": " + maxCount + "개");
+		System.out.println(arr[minIndex] + ": " + minCount + "개");
+		int total = maxCount + minCount;
+		System.out.println("답" + total);
 	}
 
 }
