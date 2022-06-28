7ab88222d91d634308e0490a8f6020a4c0590be7
keduit <keduit@keduit-pc>
2022-06-28 10:15:41 +0900

    1

---
+++ src/이차반복3/이차원반복문_프로젝트_개수찾기.java
@@ -0,0 +1,20 @@
+package 이차반복3;
+
+public class 이차원반복문_프로젝트_개수찾기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * [개수찾기]
+		 
+		  	 가장많이 들어있는 숫자개수와
+			 가장 적개 들어있는 숫자의 개수의 합을 구하시요.	
+			 100 : 1개
+			 3   : 5개 	
+			 답 : 6		
+		 */
+		
+
+	}
+
+}
