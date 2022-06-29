43e4fa73045b1b116dac61538b4bb5a83bf51ebd
keduit <keduit@keduit-pc>
2022-06-29 16:04:14 +0900

    7조

--- src/연습장/이차반복1_구구단출력.java
+++ src/연습장/이차반복1_구구단출력.java
@@ -25,10 +25,11 @@ public class 이차반복1_구구단출력 {
 		
 		for(int i = 2; i <= 9; i++)
 		{
-			System.out.println( "===" +  i + "단" + " ===");
+			System.out.println( "=== " +  i + "단" + " ===");
+			
 			for(int j = 1; j <= 9; j++)
 			{
-				System.out.println(i + " * " + j + " = " + i * j );
+				System.out.printf("%d * %d = %d \n", i , j, i * j);
 			}
 			
 		}
